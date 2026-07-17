c4b03f39a488b2da4691129b34867d0c46a39d6f
Calypso <izharat2009@gmail.com>
2026-07-17 21:36:29 +0700

    designing the pagination

--- supernova/src/main/java/supernova/pagination/Page.java
+++ supernova/src/main/java/supernova/pagination/Page.java
@@ -18,4 +18,50 @@ import java.util.List;
  */
 @Concept
 public interface Page<E> extends List<E> {
+
+    /**
+     * Gets the current number of page that this page is currently on.
+     *
+     * @return the current number of the page
+     */
+    int pageNumber();
+
+    /**
+     * Gets the parent of the page.
+     *
+     * @return a pagination
+     */
+    Pagination<E> pagination();
+
+    /**
+     * Gets the next page.
+     *
+     * @return the next page
+     */
+    Page<E> nextPage();
+
+    /**
+     * Gets the previous page.
+     *
+     * @return the previous page
+     */
+    Page<E> previousPage();
+
+    /**
+     * Returns whether this page has a next page.
+     *
+     * @return {@code true} if a next page exists; {@code false} otherwise
+     */
+    default boolean hasNext() {
+        return pageNumber() + 1 < pagination().pageCount();
+    }
+
+    /**
+     * Returns whether this page has a previous page.
+     *
+     * @return {@code true} if a previous page exists; {@code false} otherwise
+     */
+    default boolean hasPrevious() {
+        return pageNumber() > 0;
+    }
 }
--- supernova/src/main/java/supernova/pagination/Pagination.java
+++ supernova/src/main/java/supernova/pagination/Pagination.java
@@ -2,6 +2,8 @@ package supernova.pagination;
 
 import supernova.annotations.Concept;
 
+import java.util.Collection;
+
 /**
  * A collection of elements divided into fixed-size pages.
  *
@@ -19,4 +21,99 @@ import supernova.annotations.Concept;
  */
 @Concept
 public interface Pagination<E> extends Iterable<E> {
+
+    /**
+     * Gets the maximum number of elements each page can contain.
+     *
+     * @return the page size
+     */
+    int pageSize();
+
+    /**
+     * Gets the total number of the page.
+     *
+     * @return the total number of the page
+     */
+    int pageCount();
+
+    /**
+     * Checks if the pagination is empty.
+     *
+     * @return {@code true} if there was no page available
+     */
+    boolean isEmpty();
+
+    /**
+     * Add an element to the pagination.
+     *
+     * <p>It will search available page or creating new one and insert the element into the page.</p>
+     *
+     * @param element the instance of the element
+     * @return {@code true} as specified by {@link Page#add(Object)}
+     */
+    boolean add(E element);
+
+    /**
+     * Remove an element from the pagination.
+     *
+     * @param element the instance of the element
+     * @return {@code true} as specified by {@link Page#remove(Object)}
+     */
+    boolean remove(E element);
+
+    /**
+     * Remove an element at the specified index.
+     *
+     * @param index the index of the element
+     * @return {@code true} if removed
+     */
+    boolean remove(int index);
+
+    /**
+     * Returns the page with the specified page number.
+     *
+     * <p>Page numbers are one-based. The first page has a page number of
+     * {@code 1}.
+     *
+     * @param pageNumber the page number
+     * @return the page with the specified page number
+     * @throws IndexOutOfBoundsException if the page number is less than
+     *         {@code 1} or greater than {@code pageCount()}
+     */
+    Page<E> get(int pageNumber);
+
+    /**
+     * Returns the page containing the element at the specified index.
+     *
+     * @param index the index of the element
+     * @return the page containing the specified element
+     * @throws IndexOutOfBoundsException if the index is out of range
+     *         ({@code index < 0 || index >= size()})
+     */
+    Page<E> getFromIndex(int index);
+
+    /**
+     * Gets the first page of the pagination.
+     *
+     * @return the first page
+     */
+    default Page<E> getFirst() {
+        return get(1);
+    }
+
+    /**
+     * Gets the last page of the pagination.
+     *
+     * @return the last page
+     */
+    default Page<E> getLast() {
+        return get(pageCount());
+    }
+
+    /**
+     * Gets a collection of pages.
+     *
+     * @return a collection of pages.
+     */
+    Collection<Page<E>> pages();
 }
